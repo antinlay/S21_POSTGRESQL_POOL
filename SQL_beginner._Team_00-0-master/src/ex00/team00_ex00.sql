0b3dd2e45be5c53d824f17cafdd7fc3a4d1ee4d6
Janiece Eleonor <janiecee@debian-BULLSEYE-live-builder-AMD64>
2022-12-05 12:35:34 +0300

    develop TEAM DONE next DAY05

--- SQL_beginner._Team_00-0-master/src/ex00/team00_ex00.sql
+++ SQL_beginner._Team_00-0-master/src/ex00/team00_ex00.sql
@@ -1,20 +1,54 @@
--- CREATE TABLE IF NOT EXISTS graph (point1 VARCHAR, point2 VARCHAR, cost INT);
--- INSERT INTO graph(point1, point2, cost)
--- VALUES ('a', 'b', 10),
---     ('a', 'c', 15),
---     ('a', 'd', 20),
---     ('b', 'a', 10),
---     ('b', 'c', 35),
---     ('b', 'd', 25),
---     ('c', 'a', 15),
---     ('c', 'b', 35),
---     ('c', 'd', 30),
---     ('d', 'a', 20),
---     ('d', 'b', 25),
---     ('d', 'c', 30);
-WITH city1 AS (
-    SELECT DISTINCT point1
-    FROM graph
+CREATE TABLE city_and_price (
+    t1 varchar,
+    t2 varchar,
+    price integer not null default 0
+);
+INSERT INTO city_and_price
+VALUES ('a', 'b', 10),
+    ('b', 'a', 10),
+    ('a', 'd', 20),
+    ('d', 'a', 20),
+    ('a', 'c', 15),
+    ('c', 'a', 15),
+    ('b', 'c', 35),
+    ('c', 'b', 35),
+    ('d', 'b', 25),
+    ('b', 'd', 25),
+    ('d', 'c', 30),
+    ('c', 'd', 30);
+WITH RECURSIVE a_traces AS (
+    SELECT t1 AS tour,
+        t1 AS node1,
+        t2 AS node2,
+        price,
+        price AS summ
+    FROM city_and_price
+    WHERE t1 = 'a'
+    UNION ALL
+    SELECT parrent.tour || ',' || child.t1 AS trace,
+        child.t1,
+        child.t2,
+        child.price,
+        parrent.summ + child.price AS summ
+    FROM city_and_price AS child
+        INNER JOIN a_traces AS parrent ON child.t1 = parrent.node2
+    WHERE tour NOT LIKE '%' || child.t1 || '%'
+),
+CTE_SUMM (summ) AS (
+    SELECT min(summ)
+    FROM a_traces
+    WHERE length(tour) = 7
+        AND node2 = 'a'
 )
-SELECT (city1 + city2 + city3) AS total_cost,
-    CONCAT(p1, p2, p3, p4, p5) AS tour
+SELECT summ AS total_cost,
+    ('{' || tour || ',' || node2 || '}') AS tour
+FROM a_traces
+WHERE length(tour) = 7
+    AND node2 = 'a'
+    AND summ = (
+        SELECT summ
+        FROM CTE_SUMM
+    )
+ORDER BY summ,
+    tour;
+-- DROP TABLE cities, graph
