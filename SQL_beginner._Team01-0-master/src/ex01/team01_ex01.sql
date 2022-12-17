b5c8cf98158623b5209cf6d6d74d803bc6a48b59
Janiece Eleonor <janiecee@debian-BULLSEYE-live-builder-AMD64>
2022-12-17 12:09:24 +0300

    develop LAST TEAM NOT DONE!

--- SQL_beginner._Team01-0-master/src/ex01/team01_ex01.sql
+++ SQL_beginner._Team01-0-master/src/ex01/team01_ex01.sql
@@ -1,7 +1,3 @@
--- insert into currency
--- values (100, 'EUR', 0.85, '2022-01-01 13:29');
--- insert into currency
--- values (100, 'EUR', 0.79, '2022-01-08 13:29');
 CREATE FUNCTION fnc_bef_date(balance_date timestamp without time zone) RETURNS timestamp without time zone AS $$
 SELECT updated
 FROM currency
@@ -18,24 +14,38 @@ WHERE updated >= balance_date
 ORDER BY updated ASC
 LIMIT 1 $$ LANGUAGE SQL;
 -- SELECT fnc_aft_date('2022-01-08 13:29')
-SELECT fnc_nearest_rate(
-        currency.id,
-        '2022-01-08 13:29'::timestamp without time zone,
-        fnc_aft_date(balance.updated)::timestamp without time zone,
-        fnc_bef_date(balance.updated)::timestamp without time zone
-    )
-FROM currency,
-    balance;
+SELECT COALESCE(pu.name, 'not defined'),
+    COALESCE(pu.lastname, 'not defined'),
+    pc.name,
+    pc.rate_to_usd * pb.money AS currency_in_usd
+FROM public.balance pb
+    JOIN public.currency pc ON pb.currency_id = pc.id
+    JOIN public.user pu ON pu.id = pb.user_id
+WHERE fnc_nearest_rate(
+        pc.id,
+        pb.updated,
+        fnc_aft_date(pb.updated),
+        fnc_bef_date(pb.updated)
+    ) = pc.updated
+ORDER BY 1 DESC,
+    2,
+    3;
 CREATE FUNCTION fnc_nearest_rate(
-    cur_id integer,
+    cur_id bigint,
     balance_date timestamp without time zone,
     aft_date timestamp without time zone,
     bef_date timestamp without time zone
 ) RETURNS timestamp without time zone LANGUAGE PLPGSQL AS $$
-DECLARE t1 timestamp without time zone;
-t2 timestamp without time zone;
-BEGIN t1 = bef_date - balance_date;
-t2 = balance_date - aft_date;
+DECLARE t1 numeric;
+t2 numeric;
+BEGIN t1 = EXTRACT(
+    EPOCH
+    FROM (bef_date - balance_date)
+);
+t2 = EXTRACT(
+    EPOCH
+    FROM (balance_date - aft_date)
+);
 IF (t1 > t2) THEN RETURN aft_date;
 ELSE RETURN bef_date;
 END IF;
